0a2c4974f6956ed1b0b9ff10d18b364932e5de8d
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-09-25 12:32:34 +0800

    Set up model for recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  scope :search, -> (keyword) { where('title ILIKE ?', "%#{keyword}%") if keyword.present? }
+
+  validates :title, :href, presence: true
+  validates :href, uniqueness: true
 end
