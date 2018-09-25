a1a5142c76c0cbb76792c2b3958b7d80a8ef2770
thecodenomad@gmail.com <thecodenomad@gmail.com>
2018-09-25 12:55:23 +0800

    Set up views for live search on index page of recipes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,25 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  var recipe_search_form = $("#recipe-search");
+  var recipes_list = $("#recipes");
+  var recipe_search_field = $("#search");
+
+  recipe_search_form.keyup(function() {
+    recipes_list.hide();
+    if (recipe_search_field.val() !== "") {
+      recipes_list.show();
+      $.get(
+        recipe_search_form.attr("action"),
+        recipe_search_form.serialize(),
+        null,
+        "script"
+      );
+      return false;
+    } else {
+      recipes_list.hide();
+    }
+  });
+});
